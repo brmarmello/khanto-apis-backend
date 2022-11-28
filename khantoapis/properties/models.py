867bea131d634d200bde468ba68fccdc914e8092
Bruno Marmello <brmarmello@gmail.com>
2022-11-28 17:39:32 -0300

    feat: create model to Properties.

--- khantoapis/properties/models.py
+++ khantoapis/properties/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Properties(models.Model):
+    property_id = models.AutoField(primary_key=True, editable=False),
+    guest_limit = models.IntegerField(blank=False, null=False),
+    amount_retroon = models.IntegerField(blank=False, null=False),
+    accept_pet = models.BooleanField(default=True),
+    cleaning_value = models.DecimalField(max_digits=5, decimal_places=2, blank=False, null=False),
+    activation_date = models.DateField(blank=False, null=False),
+    creation_date_time = models.DateTimeField(auto_now_add=True),
+    update_date_time = models.DateTimeField(auto_now_add=True)
